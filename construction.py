837a4af651fc37cdab7d23f7fadd58b147db4269
Mehdi AMOR OUAHMED <mehdi.amor_ouahmed@g.enp.edu.dz>
2023-10-26 14:08:48 +0100

    Adding 'construction.py' file :
    This file contains the implemenation of the tree class with the construction function.

---
+++ construction.py
@@ -0,0 +1,43 @@
+### Pour exécuter le script sur terminal utilisez : python construction.py feuille1 feuille2 feuille3 ...
+
+from sha_256 import *
+class arbre_merkle:
+    def __init__(self, gauche, droit):
+        self.gauche = gauche
+        self.droit = droit
+        if isinstance(gauche, arbre_merkle):  # Si les noeud ne sont pas des feuilles, ils sont des arbres et on prends leurs racines
+            gauche = gauche.hash_
+            droit = droit.hash_
+        else:  # Si le noeuds sont des feuilles on prend leurs hashes
+            gauche = sha_256(gauche)
+            droit = sha_256(droit)
+        a = gauche
+        b = droit
+        hash_ab = sha_256(a+b)
+        self.hash_ = hash_ab
+
+def construction(liste):
+    l = len(liste)
+    if (int(l / 2) == l / 2):  # If even
+        parents = []
+        for i in range(int(l / 2)):
+            parents.append(arbre_merkle(liste[2 * i], liste[2 * i + 1]))
+    else:  # If odd
+        parents = []
+        for i in range(int(l / 2)):
+            parents.append(arbre_merkle(liste[2 * i], liste[2 * i + 1]))
+        parents.append(arbre_merkle(liste[-1], liste[-1]))
+    if len(parents) == 1:
+        return parents[0]
+    else:
+        return construction(parents)
+
+if __name__ == "__main__":
+    import sys
+
+    feuilles = sys.argv[1:]
+    arbre = construction(feuilles)
+    print("Racine de Merkle:", arbre.hash_)
+
+
+### ©Mehdi AMOR OUAHMED, Oct 2023
