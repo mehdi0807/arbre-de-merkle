7858288b64ceb0bbffd324cf8f38bafa02a67d9b
Mehdi AMOR OUAHMED <mehdi.amor_ouahmed@g.enp.edu.dz>
2023-10-26 15:32:48 +0100

    Changing the comments and the variable names
    Using English instead of French

--- verification.py
+++ verification.py
@@ -1,22 +1,22 @@
 from sha_256 import *
 from construction import *
-def verification(arbre, element):
+def verification(tree, element):
     hashes = []
-    if not isinstance(arbre.gauche, arbre_merkle):   # Si les fils sont des feuilles
-        hashes.append(arbre.hash_)
-        return element in arbre.droit or element in arbre.gauche, hashes
+    if not isinstance(tree.gauche, tree_merkle):   # If the sons are nodes
+        hashes.append(tree.hash_)
+        return element in tree.droit or element in tree.gauche, hashes
 
-    resultat_droit, hashes_droit = verification(arbre.droit, element)
-    resultat_gauche, hashes_gauche = verification(arbre.gauche, element)
+    resultat_droit, hashes_droit = verification(tree.droit, element)
+    resultat_gauche, hashes_gauche = verification(tree.gauche, element)
 
     hashes.extend(hashes_droit)
     hashes.extend(hashes_gauche)
 
     return resultat_droit or resultat_gauche, hashes
 
-def check(arbre, element):
-    resultat, hashes = verification(arbre, element)
-    if resultat:  # Si l'élément fait partie des feuilles de l'arbre on prend les hashes du chemain parcouru
+def check(tree, element):
+    resultat, hashes = verification(tree, element)
+    if resultat:  # if the element is one of the tree's node we take also the hashes where it's present
         return resultat, hashes
     else:
         return resultat
